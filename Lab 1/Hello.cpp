ada71acb0d053c89fa7a31e33b9cdbe81d4cddec
Avkorisal <anjanakorisal@gmail.com>
2025-01-29 20:45:52 -0800

    Lab 1

---
+++ Lab 1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * In this lab we are setting up our IDE, creating a GITHUB account, familiarizing CLI and compiling and executing C++ programs.
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!");
+    return 0;
+}
